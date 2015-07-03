3cc7ab1993e1906813d7040c2bd1202239bba599
HMMiller <375f3cc1@opayq.com>
2015-07-02 20:32:07 -0400

    First -> Git

---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿namespace Forte
+
+open WebSharper
+open WebSharper.Sitelets
+open WebSharper.Html.Server
+
+module Warp =
+    
+    type Endpoints = 
+        | [<EndPoint "GET /">] Home
+        | [<EndPoint "GET /about">] About
+
+    let MySite =
+        Warp.CreateApplication (fun ctx endpoint ->
+            let (=>) label endpoint = A [HRef (ctx.Link endpoint)] -< [Text label]
+            match endpoint with
+            | Endpoints.Home ->
+                Warp.Page(
+                    Body =
+                        [
+                            H1 [Text "Hello world!"]
+                            "About" => Endpoints.About
+                        
+                        ]
+                 )
+            |Endpoints.About ->
+                Warp.Page(
+                    Body =
+                        [
+                            P [Text "This is a simple app"]
+                            "Home" => Endpoints.Home
+                        ]
+                )
+            )
+
+    [<EntryPoint>]
+    do Warp.RunAndWaitForInput(MySite) |> ignore
---
+++ Warp.fsx
@@ -0,0 +1,17 @@
+﻿
+#r @"Paket.Core.dll"
+open Paket
+
+let dependencies = Dependencies.Locate(__SOURCE_DIRECTORY__)
+
+dependencies.Add("WebSharper")
+dependencies.Add("WebSharper.Warp")
+dependencies.Add("WebSharper.D3")
+dependencies.Add("WebSharper.UI.Next")
+dependencies.Add("WebSharper.Sitelets")
+dependencies.Add("WebSharper.HTML.Server")
+dependencies.Add("Fsharp.Compiler.Tools")
+
+Paket.NuGetConvert.replaceNugetWithPaket
+
+dependencies.GetInstalledPackages()
